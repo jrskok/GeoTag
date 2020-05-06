b5c6d2d9083540ee8ca1242407a20dffcf24b989
J.R. Skok <jrs222@gmail.com>
2020-05-06 12:55:00 -0700

    Update JsonTest.js

--- JsonTest.js
+++ JsonTest.js
@@ -1,17 +1,192 @@
-var map = L.map('map', {
-  'center': [0, 0],
-  'zoom': 1,
-  'layers': [
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      'attribution': 'Map data &copy; OpenStreetMap contributors'
-    })
-  ]
-});
-
-$.getJSON('JsonTest.json', function (geojson) {
-  L.geoJson(geojson, {
-    onEachFeature: function (feature, layer) {
-      layer.bindPopup(feature.properties.name);
+var dataset = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "PLANET": "Earth",
+        "NAME": "MVP Field Site",
+        "DETAILS": "MVP Field Site Info",
+        "TYPE": "POINT",
+        "PAPERS": "pap_1"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -116.190414,
+          35.18072
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "PLANET": "Mars",
+        "NAME": "FRT00010628_07_IF165S_TRR3",
+        "DETAILS": "NAN",
+        "TYPE": "CRISM",
+        "PAPERS": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          67.355,
+          9.166
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "PLANET": "Mars",
+        "NAME": "FRT00003FB9_07_IF166S_TRR3",
+        "DETAILS": "NAN",
+        "TYPE": "CRISM",
+        "PAPERS": "10.1126/science.1164759"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          78.55199999999998,
+          21.331
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "PLANET": "Mars",
+        "NAME": "HRL000040FF_07_IF183S_TRR3",
+        "DETAILS": "NAN",
+        "TYPE": "CRISM",
+        "PAPERS": "10.1126/science.1164759"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          77.413,
+          18.505
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "PLANET": "Mars",
+        "NAME": "FRT0000A09C_07_IF166S_TRR3",
+        "DETAILS": "NAN",
+        "TYPE": "CRISM",
+        "PAPERS": "10.1126/science.1164759"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          78.509,
+          21.288
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "PLANET": "Mars",
+        "NAME": "FRT000093BE_07_IF166S_TRR3",
+        "DETAILS": "NAN",
+        "TYPE": "CRISM",
+        "PAPERS": "10.1126/science.1164759"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          76.438,
+          19.222
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "PLANET": "Mars",
+        "NAME": "HRL000040FF_07_IF183S_TRR3",
+        "DETAILS": "NAN",
+        "TYPE": "CRISM",
+        "PAPERS": "10.1126/science.1164759"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          77.413,
+          18.505
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "PLANET": "Mars",
+        "NAME": "FRT0000B072_0D_IF156S_TRR3",
+        "DETAILS": "NAN",
+        "TYPE": "CRISM",
+        "PAPERS": "10.1126/science.1164759"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          78.428,
+          21.945
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "PLANET": "Mars",
+        "NAME": "FRT0000818D_07_IF165S_TRR3",
+        "DETAILS": "NAN",
+        "TYPE": "CRISM",
+        "PAPERS": "10.1029/2009JE003338"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          67.946,
+          15.913
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "PLANET": "Mars",
+        "NAME": "FRT00009D02_07_IF165S_TRR3",
+        "DETAILS": "NAN",
+        "TYPE": "CRISM",
+        "PAPERS": "10.1029/2009JE003338"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          70.009,
+          12.54
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "PLANET": "Mars",
+        "NAME": "FRT00010628_07_IF165S_TRR3",
+        "DETAILS": "NAN",
+        "TYPE": "CRISM",
+        "PAPERS": "10.1038/ngeo990"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          67.355,
+          9.166
+        ]
+      }
     }
-  }).addTo(map);
-});
+  ]
+};
